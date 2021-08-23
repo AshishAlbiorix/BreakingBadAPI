b9babbabfa4298d2fb1eca3382dca78c19f52250
Ashish <ashish.albiorix@gmail.com>
2021-08-23 19:05:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Image from "react-bootstrap/Image";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "bootstrap";
+
 function App() {
+  const [geturl, setGeturl] = useState([]);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    axios.get(`https://breakingbadapi.com/api/characters`).then((res) => {
+      setGeturl(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Breaking Bad API</h1>
+      <div className="container">
+        <div className="row">
+          {geturl.map((item, index) => (
+            <div className="col-lg-3" key={index}>
+              <div className="user-img">
+                <img src={item.img} className="img-fluid" />
+              </div>
+              <div>
+                <button onClick={()=>setShow(!show)} >{ show ? "Hide" : "Show"}</button>
+              </div>
+              {
+                show ?            
+              <div className="text-center">
+                <h4>{item.name}</h4>
+                <div className="content">
+                  {item.birthday != "Unknown" ? (
+                    <span>DOB: {item.birthday}</span>
+                  ) : (
+                    ""
+                  )}
+                  {item.occupation != "" ? (
+                    <span>Occupation: {item.occupation}</span>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+              :  ''
+              }
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
