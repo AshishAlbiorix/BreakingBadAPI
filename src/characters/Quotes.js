7e977556c34abc4d6f66e1bb7210b0812b939791
Ashish <ashish.albiorix@gmail.com>
2021-08-26 17:27:38 +0530

    Changes Done in Quotes

--- src/characters/Quotes.js
+++ src/characters/Quotes.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function Quotes() {
-    
+
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
     axios.get(`https://breakingbadapi.com/api/quotes`).then((res) => {
       setQuotes(
-        res.data.sort(() => Math.random(1) - Math.random(1)).find(() => true)
+        res.data.sort(() => Math.random() - Math.random()).find(() => true)
       );
     });
     setLoading(false);
